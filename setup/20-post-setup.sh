47bc536fab5ca42615d0fd49f0451417ac3a6ff8
Benjamin Fuu <wulfert.benjamin@googlemail.com>
2022-12-03 16:07:41 +0100

    Update 20-post-setup.sh

--- setup/20-post-setup.sh
+++ setup/20-post-setup.sh
@@ -3,49 +3,63 @@
 # This script should get called AFTER installing arch (after leaving arch-chroot and rebooting for the first time)
 #
 
+# Networking and other System Services
 
+## Networking
+pacman -S networkmanager
+
+### Enable NetworkManager
+systemctl enable NetworkManager.service
+
+## System Services
+pacman -S acpid dbus avahi cups
+
+## Enable previously installed services
+systemctl enable acpid
+systemctl enable avahi-daemon
+systemctl enable cups.service
 
 ## Tools / Optionals
-pacman -S "man-db man-pages"
-pacman -S "ntfs-3g"
-pacman -S "mc unrar unzip unarj p7zip cabextract"
-pacman -S "arch-install-scripts" # useful for getting the genfstab command :-)
+pacman -S man-db man-pages
+pacman -S ntfs-3g
+pacman -S mc unrar unzip unarj p7zip cabextract
 
 # Desktop Environment
 
 ## Install nvidia driver
 ## Hint: Don't use nouvou, it's the gate to the hell
-pacman -S "nvidia"
+pacman -S nvidia
 
 ## Desktop Environment
-pacman -S "xorg-server xorg-xinit xorg-apps xorg-xkill gdm gnome"
+pacman -S xorg-server xorg-xinit xorg-apps xorg-xkill gdm gnome
 
 ## Enable gdm
 systemctl enable gdm.service
 
 ## System
-pacman -S "zsh git nano lynx htop"
-pacman -S "xfce4-terminal menulibre gparted"
-pacman -S "noto-fonts-extra"
-pacman -S "aspell"
+pacman -S zsh git nano lynx htop
+pacman -S xfce4-terminal menulibre gparted
 
 ## Web
-pacman -S "firefox thunderbird"
+pacman -S firefox thunderbird
 
 ## Office
-pacman -S "libreoffice-still"
+pacman -S libreoffice-still aspell
 
 ## Theming
-pacman -S "gtk-engine-murrine gtk-engines gnome-tweaks papirus-icon-theme"
+pacman -S noto-fonts-extra
+pacman -S papirus-icon-theme
+pacman -S gtk-engine-murrine gtk-engines 
+pacman -S gnome-tweaks
 
 ## Android
-pacman -S "usbutils gvfs-mtp libmtp android-udev mtpfs"
+pacman -S usbutils gvfs-mtp libmtp android-udev mtpfs
 
 ## Steam / multilib
 ### Un-comment the [multilib] block in /etc/pacman.conf
 pacman -Sy
-pacman -S "steam ttf-liberation lib32-nvidia-utils"
-pacman -S "ffmpeg4.4 zenity libnotify"
+pacman -S steam ttf-liberation lib32-nvidia-utils
+pacman -S ffmpeg4.4 zenity libnotify
 
 # Install yay
 pacman -S --needed git base-devel
@@ -54,13 +68,11 @@ cd yay
 makepkg -si
 
 # Install software from aur
-yay -S "gnome-shell-extension-blur-my-shell"
+yay -S gnome-shell-extension-blur-my-shell
 
 # Spotify, borked at the moment (manually copy the sh512 checksum from mirror into pkgbuild)
 # yay -S "spotify"
 
-
-
 # Post-Post-Setups
 # Retrieve https://github.com/fthx/dashbar and install via extension-manager
-# Use pacman -S "gnome-browser-connector" to install extensions from the store"
+# Use pacman -S gnome-browser-connector to install extensions from the store"
