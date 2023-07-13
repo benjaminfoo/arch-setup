3f0383a80ea6705c3b67c5df4e8434cb827e2131
Benjamin Fuu <wulfert.benjamin@googlemail.com>
2023-07-13 11:50:53 +0200

    Create update_system.sh

---
+++ post-update/update_system.sh
@@ -0,0 +1,15 @@
+#
+# This shell-script can be used to upgrade the system, while keeping the bootloader with another efi-compatible os intact.
+#
+
+# fully upgrade the system
+pacman -Syu
+
+# generate bootloader config
+sudo grub-mkconfig -o /boot/grub/grub.cfg
+ 
+# install efi compatible bootloader
+sudo grub-install --target=x86_64-efi --bootloader-id=grub_uefi --recheck
+
+# Check and edit: /etc/default/grub
+# GRUB_DISABLE_OS_PROBER=false
