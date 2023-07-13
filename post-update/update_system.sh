456817aab19865f13e1c17491ec65afc992d9896
Benjamin Fuu <wulfert.benjamin@googlemail.com>
2023-07-13 11:53:51 +0200

    Update update_system.sh

--- post-update/update_system.sh
+++ post-update/update_system.sh
@@ -5,11 +5,13 @@
 # fully upgrade the system
 sudo pacman -Syu
 
+# Check and edit: /etc/default/grub
+# GRUB_DISABLE_OS_PROBER=false
+# This will allow the detection of windows compatible os and emit something like: 
+# "Found Windows Boot Manager on /dev/nvme0n1p1@/efi/Microsoft/Boot/bootmgfw.efi"
+
 # generate bootloader config
 sudo grub-mkconfig -o /boot/grub/grub.cfg
  
 # install efi compatible bootloader
 sudo grub-install --target=x86_64-efi --bootloader-id=grub_uefi --recheck
-
-# Check and edit: /etc/default/grub
-# GRUB_DISABLE_OS_PROBER=false
